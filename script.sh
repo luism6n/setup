4dd70d37ac6b2c800ffbe3ccc992a82205c223c0
Luís Möllmann <luism6n@gmail.com>
2024-07-13 18:21:08 -0300

    hide tee output

--- script.sh
+++ script.sh
@@ -1,13 +1,13 @@
-set -ex
+set +x
 
-wget --quiet --output-file - https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output --yes /usr/share/keyrings/1password-archive-keyring.gpg
+wget --quiet --output-file - https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
 
-sudo echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/amd64 stable main' > /etc/apt/sources.list.d/1password.list
+echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/amd64 stable main' | sudo tee /etc/apt/sources.list.d/1password.list > /dev/null
 
 sudo mkdir -p /etc/debsig/policies/AC2D62742012EA22/
-sudo wget --quiet --output-file - https://downloads.1password.com/linux/debian/debsig/1password.pol > /etc/debsig/policies/AC2D62742012EA22/1password.pol
+wget --quiet --output-file - https://downloads.1password.com/linux/debian/debsig/1password.pol | sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol > /dev/null
 sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
-wget --quiet --output-file - https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output --yes /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
+wget --quiet --output-file - https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
 
 sudo apt update && sudo apt install 1password
 
