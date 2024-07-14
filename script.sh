3a8f167b134f5a51a3b55c3ad6603e80c74b330e
Luís Möllmann <luism6n@gmail.com>
2024-07-13 22:55:39 -0300

    skip steps if already done to make it fasterrr

--- script.sh
+++ script.sh
@@ -1,15 +1,17 @@
 set -ex
 
-wget --quiet --output-document - https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --yes --output /usr/share/keyrings/1password-archive-keyring.gpg
+if ! which 1password > /dev/null; then
+    wget --quiet --output-document - https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --yes --output /usr/share/keyrings/1password-archive-keyring.gpg
 
-echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/amd64 stable main' | sudo tee /etc/apt/sources.list.d/1password.list > /dev/null
+    echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/amd64 stable main' | sudo tee /etc/apt/sources.list.d/1password.list > /dev/null
 
-sudo mkdir -p /etc/debsig/policies/AC2D62742012EA22/
-wget --quiet --output-document - https://downloads.1password.com/linux/debian/debsig/1password.pol | sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol > /dev/null
-sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
-wget --quiet --output-document - https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --yes --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
+    sudo mkdir -p /etc/debsig/policies/AC2D62742012EA22/
+    wget --quiet --output-document - https://downloads.1password.com/linux/debian/debsig/1password.pol | sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol > /dev/null
+    sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
+    wget --quiet --output-document - https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --yes --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
 
-sudo apt-get update > /dev/null && sudo apt-get install 1password > /dev/null
+    sudo apt-get update > /dev/null && sudo apt-get install 1password > /dev/null
+fi
 
 if ! pgrep 1password > /dev/null; then
     echo "run 1password and run this script again to continue"
@@ -18,7 +20,9 @@ fi
 
 cd "$HOME"
 
-sudo apt-get install git > /dev/null
+if ! which git > /dev/null; then
+    sudo apt-get install git > /dev/null
+fi
 
 if ! git status > /dev/null; then
     git init
